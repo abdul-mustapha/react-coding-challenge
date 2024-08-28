966c3ad634f4ebb29f0d8af5fdb5d59becb2ef05
Abdul Mustapha <abdulrmustapha@gmail.com>
2024-08-28 17:58:07 +0100

    added loading and error states

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,45 @@ import { getData } from "./services";
 import { Application } from "./types";
 
 function App() {
-  const [applications, setApplications] = useState<Application[]>([]);
+  const [applications, setApplications] = useState<Application[] | undefined>(
+    undefined
+  );
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchApplications = async () => {
       try {
-        const data = await getData();
-        setApplications(data);
+        setIsLoading(true);
+        setTimeout(async () => {
+          const data = await getData();
+          setApplications(data);
+          setIsLoading(false);
+        }, 2000); // added to test my loading state
       } catch (error) {
-        setError("Failed to fetch applications.");
+        setIsLoading(false);
+        setError("Something went wrong...");
       }
     };
 
     fetchApplications();
   }, []);
 
-  return (
-    <>
-      {JSON.stringify(applications)}
-      {JSON.stringify(error)}
-      <h1>React Coding Exercise</h1>
-      <Navigation />
-    </>
-  );
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (applications) {
+    return (
+      <div>
+        <h1>React Coding Exercise</h1>
+        {JSON.stringify(applications)}
+        <Navigation />
+      </div>
+    );
+  }
+
+  return <div>{JSON.stringify(error)}</div>;
 }
 
 export default App;
